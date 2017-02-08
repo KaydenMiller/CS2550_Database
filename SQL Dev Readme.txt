fde7750f44b7d84de8f6776b6a332430bda820e6
Kayden Miller <kaydenmiller1@gmail.com>
2017-02-08 13:09:18 -0700

    Added Assignment 3

--- SQL Dev Readme.txt
+++ SQL Dev Readme.txt
@@ -13,3 +13,5 @@ Parenthasis, AND, OR
 SQL Developer Version: 3.0.04 newer than that and you could get issues with what it outputs
 
 ||' '|| will allow strings to be on the same line but is different than the concat character
+
+GitHub Repo: https://github.com/KaydenMiller/CS2550_Database.git
--- Test.sql
+++ Test.sql
@@ -27,3 +27,30 @@ SELECT ProdName, ProdMFG, To_Char(ProdNextShipDate, 'WW W Day Month DD, YYYY HH2
 FROM Product
 WHERE ProdNextShipDate < To_Date('Feb 01 2007', 'Mon DD YYYY')
 ORDER BY ProdNextShipDate;
+
+-- SYSDATE: pulls from the systems date
+-- FROM dual -> dual is a temp table given to us by orical
+SELECT TO_CHAR(SYSDATE, 'Mon DD YYYY HH:MI:SS') AS Time
+FROM dual;
+
+-- Dates can have mathmatics done to them
+SELECT SYSDATE + 1 AS Time
+FROM dual;
+
+SELECT TO_CHAR(SYSDATE + 12/24, 'MM-DD-YYYY HH24:MI AM') AS Time
+FROM dual;
+
+SELECT EXTRACT(MONTH FROM SYSDATE) + 1 AS Month
+FROM dual;
+
+SELECT ADD_MONTHS(SYSDATE, 3) AS Future
+FROM dual;
+
+-- SELECT ROUND(MONTHS_BETWEEN(SYSDATE, created_date), 2)
+SELECT TO_CHAR(MONTHS_BETWEEN(SYSDATE, registration_date), '999.99')
+FROM student;
+
+
+
+
+
